1964554fc74691cd814b8e65f698973215dab69b
pawarshivamd <shivam.p@konzeptsolutions.com>
2023-06-23 12:06:45 +0530

    --logout

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,27 +16,47 @@ document.addEventListener('DOMContentLoaded', function () {
 
 const allSideMenu = document.querySelectorAll('#side_nav .ul-section.top li .link-box');
 
-allSideMenu.forEach(item=> {
-	const li = item.parentElement;
+allSideMenu.forEach(item => {
+    const li = item.parentElement;
 
-	item.addEventListener('click', function () {
-		allSideMenu.forEach(i=> {
-			i.parentElement.classList.remove('active');
-		})
-		li.classList.add('active');
-	})
+    item.addEventListener('click', function () {
+        allSideMenu.forEach(i => {
+            i.parentElement.classList.remove('active');
+        })
+        li.classList.add('active');
+    })
 });
 function myCopyText() {
     // Get the text field
     var copyText = document.getElementById("myInput");
-  
+
     // Select the text field
     copyText.select();
     copyText.setSelectionRange(0, 99999); // For mobile devices
-  
-     // Copy the text inside the text field
+
+    // Copy the text inside the text field
     navigator.clipboard.writeText(copyText.value);
-  
+
     // Alert the copied text
     alert("Copied the text: " + copyText.value);
-  }
+}
+
+
+window.addEventListener('scroll', function () {
+    const viewportWidth = window.innerWidth;
+    const sidebar = document.getElementById('side_nav');
+
+    if (viewportWidth <= 767) {
+        sidebar.classList.add('sidebar');
+        sidebar.style.position = 'fixed';
+        sidebar.style.top = '0';
+        sidebar.style.height = '100vh';
+        sidebar.style.overflowY = 'auto';
+    } else {
+        sidebar.classList.add('sidebar');
+        sidebar.style.position = 'sticky';
+        sidebar.style.top = '0';
+        sidebar.style.height = '100vh';
+        sidebar.style.overflowY = 'auto';
+    }
+});
